f1d00b525bdf434cb3bc2ba9a758d1fe1b416e54
unknown <yannnathan825@gmail.com>
2024-08-13 06:44:50 +0200

    Nathan

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './Components/Authentification/SignIn';
 import Login from './Components/Authentification/Login';
 import Home from './Components/Home/Home';
+import '../src/Components/Historique';
+
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route exact path="/" Component={SignIn} />
+          <Route exact path="/home" Component={SignIn} />
           <Route exact path="/login" Component={Login} />
-          <Route exact path="/home" Component={Home} />
+          <Route exact path="/" Component={Home} />
         </Routes>
     </BrowserRouter>
+    
   );
 }
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,38 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { onAuthStateChanged, signOut } from "firebase/auth";
-import {auth, db} from "../Firebase";
+import { auth, db } from "../Firebase";
 import { ref, onValue } from "firebase/database";
 
-const Home = () =>
-{
+
+const Home = () => {
     const navigate = useNavigate();
-    const anonymosUser = 
+    const anonymosUser =
     {
-        uid : "",
-        email : "",
-        username : "",
+        uid: "",
+        email: "",
+        username: "",
     }
     const [user, setUser] = useState(anonymosUser);
-    useEffect(() =>{
+    useEffect(() => {
         onAuthStateChanged(auth, (u) => {
-            if(u)
-            {
+            if (u) {
                 onValue(ref(db, '/users/' + u.uid), (snapshot) => {
-                    setUser({uid: u.uid, email: snapshot.val().email, username: snapshot.val().username });
-                  }, {
+                    setUser({ uid: u.uid, email: snapshot.val().email, username: snapshot.val().username });
+                }, {
                     onlyOnce: true
-                  })
+                })
             }
-            else 
-            {
+            else {
                 navigate("/");
             }
         })
     }, [])
 
-    const handleLogOut = () =>
-    {
+    const handleLogOut = () => {
         signOut(auth).then(() => {
             navigate("/");
         })
