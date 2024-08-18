5f99b316923c5f03281c3f59722b9e62f15d2ba0
HKB <tahinaandriantsoa2004@gmail.com>
2024-08-18 16:09:59 +0200

    modi Tahina

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignIn from './Components/Authentification/SignIn';
 import Login from './Components/Authentification/Login';
 import Home from './Components/Home/Home';
+import Manage from './Components/Home/Manage';
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
           <Route exact path="/home" Component={SignIn} />
+          <Route exact path="/manage" Component={Manage} />
           <Route exact path="/login" Component={Login} />
           <Route exact path="/" Component={Home} />
         </Routes>
